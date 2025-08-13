a83b4eac5433f356d5a1c859192d73f223087750
Fraser Dwyer <fraserd1413@gmail.com>
2025-08-13 19:11:27 +0100

    Styling added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,26 @@
 import ListItem from './components/ListItem'
 import AddItem from './components/AddItem'
-import { useState } from 'react';
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState([])
 
   const handleAddItem = (newItem: string) => {
-    setItems((prev) => [...prev, newItem]);
-  };
+    setItems((prev) => [...prev, newItem])
+  }
 
   return (
     <div className='shoppingListContainer'>
       <h1>Shopping List</h1>
       <div className='itemsContainer'>
-        { items.map((item) => {
-          return <ListItem itemName={item} />
-        })}
+        {items.length === 0 ? (
+          <p className='informationMsg'>No items on the shopping list - try adding one</p>
+        ) : (
+          items.map((item) => {
+            return <ListItem itemName={item} />
+          })
+        )}
       </div>
       <AddItem onAddItem={handleAddItem} />
     </div>
