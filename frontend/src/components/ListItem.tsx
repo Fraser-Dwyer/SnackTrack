51d60ba43ab55505bca52f720a2b8392de0fa6aa
Fraser Dwyer <fraserd1413@gmail.com>
2025-08-13 19:51:45 +0100

    Delete button styling added

--- frontend/src/components/ListItem.tsx
+++ frontend/src/components/ListItem.tsx
@@ -1,10 +1,16 @@
 import styles from './ListItem.module.css'
+import { FaRegTrashAlt } from 'react-icons/fa'
 
 function ListItem({ itemName }: { itemName: string }) {
   return (
-    <p className={styles.shoppingListItem} data-testid='listItem'>
-      {itemName}
-    </p>
+    <div className={styles.listItemContainer}>
+      <p className={styles.shoppingListItem} data-testid='listItem'>
+        {itemName}
+      </p>
+      <button className={styles.deleteButton}>
+        <FaRegTrashAlt size={30} />
+      </button>
+    </div>
   )
 }
 
