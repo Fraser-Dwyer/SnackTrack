e07605cbe8bc78b0601ef5f5326487b27578ecf3
Fraser Dwyer <fraserd1413@gmail.com>
2025-08-13 19:17:36 +0100

    Unit tests

--- frontend/src/components/ListItem.test.tsx
+++ frontend/src/components/ListItem.test.tsx
@@ -1,13 +1,12 @@
-import { render, screen } from "@testing-library/react";
-import ListItem from "./ListItem";
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import ListItem from './ListItem'
 
-describe("ShoppingListItem - Unit Tests", () => {
-  const mockItem = "Apples"
+describe('ShoppingListItem - Unit Tests', () => {
+  const mockItem = 'Apples'
 
-  it("renders the item name", () => {
-    render(<ListItem itemName={mockItem} />);
-    expect(screen.getByText("Apples")).toBeInTheDocument();
-  });
-
+  it('Renders the item name', () => {
+    render(<ListItem itemName={mockItem} />)
+    expect(screen.getByText('Apples')).toBeInTheDocument()
+  })
 })
