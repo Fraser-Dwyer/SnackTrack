7203270a8fb82a2ba125de16f9d067df92c27691
Fraser Dwyer <fraserd1413@gmail.com>
2025-08-14 16:27:31 +0100

    Fix existing tests

--- frontend/src/components/ListItem.test.tsx
+++ frontend/src/components/ListItem.test.tsx
@@ -1,19 +1,21 @@
 import { render, screen, fireEvent } from '@testing-library/react'
+import { Item } from '../types/Item'
 import '@testing-library/jest-dom'
 import ListItem from './ListItem'
 
 describe('ShoppingListItem - Unit Tests', () => {
-  const mockItem = 'Apples'
+  const mockItem: Item = {name: 'Apples', checked: false}
   const mockOnDelete = jest.fn()
+  const mockOnToggle = jest.fn()
 
   test('Renders the item name', () => {
-    render(<ListItem itemName={mockItem} onDeleteItem={mockOnDelete} />)
+    render(<ListItem item={mockItem} onDeleteItem={mockOnDelete} onToggleItem={mockOnToggle} />)
     expect(screen.getByText('Apples')).toBeInTheDocument()
   })
 
   test('calls onDeleteItem with correct item', () => {
     const mockOnDelete = jest.fn()
-    render(<ListItem itemName={'Apples'} onDeleteItem={mockOnDelete} />)
+    render(<ListItem item={mockItem} onDeleteItem={mockOnDelete} onToggleItem={mockOnToggle} />)
 
     fireEvent.click(screen.getByLabelText(/delete-Apples/i))
 
